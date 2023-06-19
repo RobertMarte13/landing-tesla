98415e5d3954a5e1071f56b71053e7cc9fa2c89e
RobertMarte13 <rproject133gmail.com>
2023-06-19 10:26:05 -0400

    pruebasss

--- src/index.js
+++ src/index.js
@@ -27,6 +27,5 @@ app.use((req, res) => {
 })
 
 
-app.listen(app.get('port'), () => {
-    console.log(`Servidor rodando na porta ${app.get('port')}`)
-})
+app.listen(process.env.PORT || app.get('port'))
+console.log(`Servidor rodando na porta ${process.env.PORT || app.get('port')}`)
