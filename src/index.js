171cdeb3e6e05540f9302d3e476b06bcbfd8e125
RobertMarte13 <rproject133gmail.com>
2023-06-19 10:35:42 -0400

    Pruebass

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const app = express()
 // setting
 app.set('port', 3000)
 app.set('nameApp', 'App Express')
-app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // middlewares
 app.use(morgan('dev'))
@@ -20,12 +20,12 @@ app.use(morgan('dev'))
 app.use(homePage)
 
 // Routes Public
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('public', express.static(path.join(__dirname, 'public')))
 
 app.use((req, res) => {
     res.status(404).send('Not found')
 })
 
 
-app.listen(process.env.PORT || app.get('port'))
-console.log(`Servidor rodando na porta ${process.env.PORT || app.get('port')}`)
+app.listen(process.env.PORT || 3000)
+console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`)
