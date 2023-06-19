9c8ee57ef837b857e847312ee85c21ab701fef5f
RobertMarte13 <rproject133gmail.com>
2023-06-19 09:44:36 -0400

    Pruebas

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,11 +2,11 @@ const { Router } = require('express')
 
 const routes = Router()
 
-routes.get('/landing-page-de-tesla.netlify.app', (req, res) => {
+routes.get('https://landing-page-de-tesla.netlify.app', (req, res) => {
     res.render('index')
 })
 
-routes.get('/landing-page-de-tesla.netlify.app/home', (req, res) => {
+routes.get('https://landing-page-de-tesla.netlify.app/home', (req, res) => {
     res.render('index')
 })
 
